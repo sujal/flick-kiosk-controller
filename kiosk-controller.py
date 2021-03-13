46ba573ec3065f8e5e8a51bf1c3f8df581e331fb
Sujal Shah <sujal@sujal.net>
2021-03-13 00:46:39 -0500

    add interval, fix mappings, other cleanup

--- kiosk-controller.py
+++ kiosk-controller.py
@@ -4,28 +4,38 @@
 
 import flicklib
 import subprocess
+import time
 
 global next_move
 global prev_move
 global last_command
 global command_count
+global last_reload_time
+global min_reload_interval
 
-next_move = 'NS'
-prev_move = 'SN'
 last_command = ''
 command_count = 0
+last_reload_time = time.time()
+
+# Initialize mappings
+next_move = 'SN'
+prev_move = 'NS'
+min_reload_interval = 2
+
 
 def issue_xdo_command(command):
 
     global last_command
+    global last_reload_time
     last_command = command
 
     xdo_command = ['xdotool']
 
-    if command == 'reload':
+    if command == 'reload' and (time.time()-last_reload_time) > min_reload_interval:
+	last_reload_time = time.time()
         xdo_command.append('key')
         xdo_command.append('--clearmodifiers')
-        xdo_command.append('ctrl-r')
+        xdo_command.append('ctrl+r')
     else:
         if command == 'next_desktop':
             xdo_command.append('set_desktop')
@@ -37,7 +47,8 @@ def issue_xdo_command(command):
             xdo_command.append('--')
             xdo_command.append('-1')
 
-    if xdo_command.length() > 1:
+    if len(xdo_command) > 1:
+        print('sending command: ', ' '.join(xdo_command))
         subprocess.call(xdo_command)
 
 
